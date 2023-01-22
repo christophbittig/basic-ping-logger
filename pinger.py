31b2806ccb35948a5657d70f26a4083df1a570f0
crocokyle <me@kyle-dixon.com>
2023-01-22 16:28:14 -0500

    feat: :sparkles: Add argparse arguments

--- pinger.py
+++ pinger.py
@@ -1,9 +1,12 @@
+import argparse
 from datetime import datetime, timedelta
+import re
+import subprocess
+
 from openpyxl.chart import LineChart, Reference
 from openpyxl import Workbook, load_workbook
 import pandas as pd
-import re
-import subprocess
+
 
 def run_command(command: str) -> str:
     raw_output = subprocess.run(command, stdout=subprocess.PIPE)
@@ -64,16 +67,14 @@ def save_data(data: pd.DataFrame, filename: str) -> bool:
         print(f"{datetime.now()} | WARNING: Failed to save with exception:\n{e}")
         return False
 
-if __name__ == "__main__":
-    start_time = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-    save_filename = f"ping_data_{start_time}.xlsx"
+def main_loop(save_filename:str, target: str, save_frequency: int):
     print(f"Starting ping logger at {datetime.now()}...")
     my_data = pd.DataFrame(columns=['Time', 'Latency', 'Packet Dropped'])
-
     while True:
         time_when_command_started = datetime.now()
-        ping_response: str = run_command(['ping', "8.8.8.8"])
+        ping_response: str = run_command(['ping', target])
         response_times: list[int] = parse_latency(ping_response)
+        # Not used now but could be used in a rolling average at some point
         packet_loss: int = parse_packet_loss(ping_response)
         
         ping_count = 1
@@ -86,5 +87,21 @@ if __name__ == "__main__":
             my_data.loc[len(my_data.index)] = [ping_time, latency, packet_dropped]
             ping_count += 1
     
-        if len(my_data) % 32 == 0: # Must be a multiple of four since ping responses have four values
+        if len(my_data) % save_frequency == 0: # Should be a multiple of four since ping responses have four values
             save_data(my_data, save_filename)
+
+if __name__ == "__main__":
+    start_time = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    save_filename = f"ping_data_{start_time}.xlsx"
+    
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-t','--target', type=str, default='8.8.8.8', help='The host or IP address you want to test against.')
+    parser.add_argument('-f', '--save-frequency', type=int, default=32, help='Number of pings to run between file saves. Should be a multiple of four.')
+    parser.add_argument('-o', '--output-file', type=str, default=f"ping_data_{start_time}.xlsx", help='Output excel file.')
+    args = parser.parse_args()
+    if args.save_freqency % 4 != 0:
+        raise ValueError(f"--save-frequency must be a multiple of four. You provided {args.save_frequency}.")
+
+    main_loop(args.output_file, target=args.target, save_frequency=args.save_frequency)
+    
